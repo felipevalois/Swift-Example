28ac04429aac96ccf397def4ff2dbeecc1a21f45
Felipe costa <felipevalois_@hotmail.com>
2019-02-08 21:14:56 -0600

    Created new project

---
+++ swift-example/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-example
+//
+//  Created by Felipe Costa on 2/8/19.
+//  Copyright © 2019 Felipe Costa. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
